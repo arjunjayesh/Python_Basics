24af4f16d4b5cf25a81c4b89c42968d95f16edbb
Arjun Jayesh <9997arjun@gmail.com>
2022-07-31 14:24:50 +0530

    Button and Frame

--- Tkinter_Program.py
+++ Tkinter_Program.py
@@ -1,3 +1,14 @@
-from tkinter import Tk
+from tkinter import *
 root=Tk()
+
+# Label
+label=Label(root,text="Hello World").pack()
+frame1=Frame(root)
+frame1.pack()
+
+# Button
+but=Button(root,text="Login",fg="red",bg="black")
+but.pack()
+
+
 root.mainloop()
