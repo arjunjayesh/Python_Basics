bed7659af932e9c3d6c597b565864434349408bc
Arjun Jayesh <9997arjun@gmail.com>
2022-08-08 12:08:17 +0530

    Tkinter using Class

---
+++ Tkinter_Using_Class.py
@@ -0,0 +1,17 @@
+from tkinter import *
+class myfun:
+    def __init__(self,rootone):
+        frame=Frame(rootone)
+        frame.pack()
+        self.pbutton=Button(frame,text='click', command=self.pmsg)
+        self.pbutton.pack()
+
+        self.qbutton=Button(frame,text="exit", command=frame.quit)
+        self.qbutton.pack(side=LEFT)
+
+    def pmsg(self):
+        print("Clicked")
+
+root=Tk()
+obj=myfun(root)
+root.mainloop()
